67927ab572dddd6e2e2d7526b96605517a948815
Andy Keller <andy@barrfitz.com>
2022-12-03 13:37:46 -0600

    fix: migrate from gatsbyjs

---
+++ lib/config.ts
@@ -0,0 +1,15 @@
+// https://nextjs.org/docs/migrating/from-gatsby
+// src/config.js
+
+const config = {
+  title: 'Starter Blog',
+  author: {
+    name: 'Lee Robinson',
+    summary: 'who loves Next.js.',
+  },
+  description: 'A starter blog converting Gatsby -> Next.',
+  social: {
+    twitter: 'leeerob',
+  },
+}
+export default config;
---
+++ pages/blog/[slug].tsx
@@ -0,0 +1,36 @@
+// src/pages/[slug].js
+
+// Install remark and remark-html
+import { remark } from 'remark'
+import html from 'remark-html'
+import { getPostBySlug, getAllPosts } from '../../lib/blog'
+
+export async function getStaticProps({ params }) {
+  const post = getPostBySlug(params.slug)
+  const markdown = await remark()
+    .use(html)
+    .process(post.content || '')
+  const content = markdown.toString()
+
+  return {
+    props: {
+      ...post,
+      content,
+    },
+  }
+}
+
+export async function getStaticPaths() {
+  const posts = getAllPosts()
+
+  return {
+    paths: posts.map((post: any) => {
+      return {
+        params: {
+          slug: post.slug,
+        },
+      }
+    }),
+    fallback: false,
+  }
+}
