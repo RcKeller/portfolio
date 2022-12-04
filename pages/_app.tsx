f2c7b6efb242d3968762eee9abbf7f0507b374bb
Andy Keller <andy@barrfitz.com>
2022-12-04 10:35:16 -0600

    feat: dark theme and switcher

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,73 @@
 import "prismjs/themes/prism-okaidia.css"
 import '../styles/globals.css'
-import "../styles/index.scss";
-import "../styles/template.scss";
+// import "../styles/index.scss";
+// import "../styles/template.scss";
+
+
 
 import type { AppProps } from 'next/app'
+// 1. Import `createTheme`
+import { createTheme, NextUIProvider } from "@nextui-org/react"
+import { ThemeProvider as NextThemesProvider } from 'next-themes';
+
+// https://fontawesome.com/docs/web/use-with/react/use-with
+import { config } from '@fortawesome/fontawesome-svg-core'
+import '@fortawesome/fontawesome-svg-core/styles.css'
+config.autoAddCss = false
+
+// 2. Call `createTheme` and pass your custom values
+const lightTheme = createTheme({
+  type: 'light',
+  theme: {
+    colors: {
+      primary: '#A020F0'
+    }
+    // colors: {...}, // optional
+  }
+})
+
+const darkTheme = createTheme({
+  type: 'dark',
+  theme: {
+    colors: {
+      // primary: '#A020F0'
+      // brand colors
+      primaryLight: '$purple200',
+      primaryLightHover: '$purple300',
+      primaryLightActive: '$purple400',
+      primaryLightContrast: '$purple600',
+      primary: '$purple700',//
+      primaryBorder: '$purple500',
+      primaryBorderHover: '$purple600',
+      primarySolidHover: '$purple700',
+      primarySolidContrast: '$white',
+      primaryShadow: '$purple500',
+
+      gradient: 'linear-gradient(112deg, $blue100 -25%, $pink500 -10%, $purple500 80%)',
+      link: '#5E1DAD',
+    }
+    // colors: {...}, // optional
+  }
+})
+
+
+// 3. Wrap NextUIProvider with NextThemesProvider
+// _app.jsx // _app.tsx
+
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <NextThemesProvider
+    defaultTheme="system"
+    attribute="class"
+    value={{
+      light: lightTheme.className,
+      dark: darkTheme.className
+    }}
+  >
+  <NextUIProvider>
+    <Component {...pageProps} />
+    </NextUIProvider>
+    </NextThemesProvider>
+  )
 }
---
+++ pages/switchable.tsx
@@ -0,0 +1,44 @@
+// 4. Use `next-themes` to change the theme
+import { useTheme as useNextTheme } from 'next-themes'
+// import { Switch, useTheme } from '@nextui-org/react'
+import { Navbar, Button, Link, Text, Switch, useTheme } from "@nextui-org/react";
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+// import { faGithub } from '@fortawesome/free-solid-svg-icons'
+
+export default function Switchable() {
+  const { setTheme } = useNextTheme();
+  const { isDark, type } = useTheme();
+
+  return (
+    <div>
+    <Navbar shouldHideOnScroll isBordered={isDark} variant="sticky">
+        <Navbar.Brand>
+          {/* <AcmeLogo /> */}
+          {/* <FontAwesomeIcon icon={fa0} /> */}
+          {/* <FontAwesomeIcon icon="fa-github" /> */}
+          <FontAwesomeIcon icon="fa-brands fa-github" />
+          <Text b color="inherit" hideIn="xs">
+            ACME
+          </Text>
+        </Navbar.Brand>
+        <Navbar.Content hideIn="xs" variant="underline">
+          <Navbar.Link isActive href="/switchpick">Projects</Navbar.Link>
+          <Navbar.Link href="/testimonials">Testimonials</Navbar.Link>
+        </Navbar.Content>
+        <Navbar.Content>
+          <Navbar.Link color="inherit" href="#">
+            Login
+          </Navbar.Link>
+          <Navbar.Item>
+            <Switch
+        checked={isDark}
+        onChange={(e) => setTheme(e.target.checked ? 'dark' : 'light')}
+        />
+          </Navbar.Item>
+        </Navbar.Content>
+    </Navbar>
+      The current theme is: {type}
+    </div>
+  )
+}
