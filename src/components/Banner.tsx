e7dc56f3657e83f80d2705f6823665b39e444961
Andy Keller <andy@barrfitz.com>
2023-03-31 15:57:51 -0500

    feat: complete bio

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -3,13 +3,24 @@ const Banner = () => (
   <section id="banner" className="major">
     <div className="inner">
       <header className="major">
-        <h1>Hi, my name is Forty</h1>
+        <h1>👋 Hi - I&apos;m Andy Keller</h1>
       </header>
-      <div className="content">
+      {/* <div className="content"> */}
+      <div className="">
         <p>
-          A responsive site template designed by HTML5 UP
-          <br />
-          and released under the Creative Commons.
+          I&apos;m a Sr. Frontend Engineer @Shopify based out of St. Louis, MO.
+          For the past decade, I have pushed myself to explore many technical
+          verticals including quantitative science, web development, DevOps and
+          cybersecurity. I began my career as a researcher in the midwest and
+          moved to Seattle to gain industry experience and an advanced
+          education, taking some time in-between to intern in Washington, DC.
+        </p>
+        <p>
+          What keeps me going is the drive to not just consume knowledge, but
+          create it. As developers we are always on the forefront of the
+          industry, creating new systems and experiences that drive social
+          progress, and it&apos;s an exciting field to be in. Our work is a
+          labor of love and I want to share that with everyone possible.
         </p>
         <ul className="actions">
           <li>
